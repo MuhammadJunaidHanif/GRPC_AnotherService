e087daa0bb0ba620d6f2b4cade609c87beb76e5b
Juanid Hanif <jhanif@social27.com>
2021-07-25 07:37:50 +0500

    Add project files.

---
+++ AnotherService/Controller/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SomeService.Protos;
+
+namespace AnotherService.Controller
+{
+    [Route("api/")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly SomeServiceApi.SomeServiceApiClient _someServiceApiClient;
+
+
+        public CalculatorController(SomeServiceApi.SomeServiceApiClient someServiceApiClient) =>
+            _someServiceApiClient = someServiceApiClient;
+
+
+        [HttpGet("calc/add")]
+        public async Task<int> AddNumbersAsync(int a, int b, CancellationToken ct) =>
+            (await _someServiceApiClient.AddNumbersAsync(
+                new AddNumbersRequest {
+                    NumA = a,
+                    NumB = b
+                },
+                cancellationToken: ct
+            )).Sum;
+
+        [HttpPost("calc/add")]
+        public async Task<int> AddNumbersAsync([FromBody] int[] numbers, CancellationToken ct) =>
+            (await _someServiceApiClient.AddNumbersFromArrayAsync(
+                new AddNumbersFromArrayRequest {
+                    Numbers = { numbers }
+                },
+                cancellationToken: ct
+            )).Sum;
+    }
+}
